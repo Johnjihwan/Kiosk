d3cad74f12e39c3c4412c9eb565bd1f204ad2cc0
joenjihwan <s20062@gsm.hs.kr>
2020-12-01 17:46:33 +0900

    [UPDATE] update admin page interface.

--- src/AdminPage/AdminPage.java
+++ src/AdminPage/AdminPage.java
@@ -7,17 +7,18 @@ class AdminPage {
 
     public static void main(String[] args) {
 //        아이템 항목 정의
-        String[] items = {"One", "Two", "Three", "Four", "Five"};
+        String[] items = {"정식", "디저트", "음료", "추가상품"};
         JComboBox combo = new JComboBox(items);
-        JTextField field1 = new JTextField("0");
-        JTextField field2 = new JTextField("0");
+        JTextField field1 = new JTextField("상품명을 입력하세요");
+        JTextField field2 = new JTextField("가격을 입력하세요");
         JPanel panel = new JPanel(new GridLayout(0, 1));
 
 //        ui에 들어갈 아이템들을 정의
+        panel.add(new JLabel("Category"));
         panel.add(combo);
-        panel.add(new JLabel("상품명:"));
+        panel.add(new JLabel("Product"));
         panel.add(field1);
-        panel.add(new JLabel("Field 2:"));
+        panel.add(new JLabel("Value"));
         panel.add(field2);
 
 //        결과 값 console로 반환.
